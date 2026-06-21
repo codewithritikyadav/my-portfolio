9359abaae938b35dd48d27c243a549aa64101dcc
codewithritikyadav <ritikyadav11741@gmail.com>
2026-06-22 00:33:45 +0530

    fixed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = []
+ALLOWED_HOSTS = ['*']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
